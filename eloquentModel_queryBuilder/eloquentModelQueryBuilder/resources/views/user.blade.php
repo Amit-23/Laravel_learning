a4e4d057bffd0133ab815831bfb1b8c6a71dd0fd
Amit-23 <amitsanghalsingh@gmail.com>
2024-09-28 13:49:35 +0530

    New file

---
+++ eloquentModel_queryBuilder/eloquentModelQueryBuilder/resources/views/user.blade.php
@@ -0,0 +1,20 @@
+<div>
+   
+    <h1>User data</h1>
+
+    <table border="1">
+        <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+        </tr>
+        @foreach($users as $user)
+        <tr>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->phone}}</td>
+        </tr>
+        @endforeach
+
+    </table>
+</div>
